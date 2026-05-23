9c6b6d4822cc898b44731d8abd5198e9ba83361f
arten162 <ar.zemlanoj5004@gmail.com>
2026-05-23 15:13:49 +0300

    new pushes

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,6 +2,7 @@ import pytest
 import sqlite3
 import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from io import StringIO
 from registration.registration import create_db, add_user, authenticate_user, display_users, user_choice, main
 
